955dfb02c6170102becab8fec259355888b37266
“anassadiqui” <anass.sadiqui@gmail.com>
2023-06-15 15:53:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art.\n");
+	return (0);
+}
